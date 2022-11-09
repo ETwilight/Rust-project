7148dc4e94ff3266f8c4891ba7aa272a7b2beaea
CharlieDreemur <zhangwj.charlie@gmail.com>
2022-11-08 21:29:49 -0600

    Project Setup and Rocker Setup

---
+++ rust_kill/target/debug/deps/rust_kill-2fab3afe428874a9.d
@@ -0,0 +1,5 @@
+d:\StudyLife\Github\Rust-project\rust_kill\target\debug\deps\rust_kill-2fab3afe428874a9.rmeta: src\main.rs
+
+d:\StudyLife\Github\Rust-project\rust_kill\target\debug\deps\rust_kill-2fab3afe428874a9.d: src\main.rs
+
+src\main.rs:
