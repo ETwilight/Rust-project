0ce98265575bc7bf2aca8944af5d6b04c087e5b0
ETwilight <hellobrian18@163.com>
2022-12-06 04:15:55 -0600

    Send Sender<Message> to client

--- rust_kill/src/client/room.rs
+++ rust_kill/src/client/room.rs
@@ -4,10 +4,11 @@ mod utils;
 #[path="../game/game_info.rs"]
 mod game_info;
 
-use crate::client::room::game_info::Player;
+use rocket::tokio::sync::broadcast::Sender;
+use crate::{client::room::game_info::Player, Message};
 
 // tell the javascript that it should connect to an empty room with name given in the room info
-pub async fn connectRoom(name: String) {
+pub async fn connectRoom(name: String, sender: Sender<Message>) {
     print!("Successfully connect to room: {} \n", name);
 }
 
