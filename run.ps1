d0da67d4b8a8cec0a955b04e26f9a9c3605cfe09
ETwilight <hellobrian18@163.com>
2022-12-10 02:04:21 -0600

    Make testing easier

--- run.ps1
+++ run.ps1
@@ -1,4 +1,6 @@
 #!/bin/bash
+$urls = "127.0.0.1:8000", "127.0.0.1:8001", "127.0.0.1:8002", "127.0.0.1:8003", "127.0.0.1:8004", "127.0.0.1:8005"
+
 $commands =
 "cd rust_kill",
 "Start-Process cargo 'run 8000 true'",
@@ -6,9 +8,13 @@ $commands =
 "Start-Process cargo 'run 8002 false'",
 "Start-Process cargo 'run 8003 false'",
 "Start-Process cargo 'run 8004 false'",
-"Start-Process cargo 'run 8005 false'"
+"Start-Process cargo 'run 8005 false'",
+"Start-Sleep -Seconds 3.0"
 # Run the commands using the iex cmdlet
 foreach ($command in $commands) {
     iex $command
 }
-done
+
+foreach ($url in $urls) {
+    Start-Process chrome.exe -ArgumentList "-new-tab $url"
+}
--- run.sh
+++ run.sh
@@ -1,10 +1,31 @@
 #!/usr/bin/env sh
 
-# Define the commands to run
-commands="cd rust_kill; cargo run 8000 true; cargo run 8001 false; cargo run 8002 false; cargo run 8003 false; cargo run 8004 false; cargo run 8005 false"
+urls=(
+"127.0.0.1:8000"
+"127.0.0.1:8001"
+"127.0.0.1:8002"
+"127.0.0.1:8003"
+"127.0.0.1:8004"
+"127.0.0.1:8005"
+)
 
-# Change to the rust_kill directory
-cd rust_kill
+commands=(
+"cd rust_kill"
+"cargo run 8000 true"
+"cargo run 8001 false"
+"cargo run 8002 false"
+"cargo run 8003 false"
+"cargo run 8004 false"
+"cargo run 8005 false"
+"sleep 3.0"
+)
 
-# Run the commands using the & operator
-$commands &
+for command in "${commands[@]}"
+do
+    $command
+done
+
+for url in "${urls[@]}"
+do
+    xdg-open "$url"
+done
