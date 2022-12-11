ed402995d45509455ebb562c3cdc1bf88ff40d44
Cobbi <cobbi2004@gmail.com>
2022-12-11 07:12:22 -0600

    lobb.js

--- rust_kill/static/Lobby.js
+++ rust_kill/static/Lobby.js
@@ -1,5 +1,3 @@
-let hform = document.querySelector('#hostform');
-let cForm = document.querySelector('#clientform');
 
 function openClient() {
   document.getElementById("f2").style.display = "block";
@@ -18,13 +16,15 @@ function closeHost() {
 }
 
 function ChangePage() {
-  location.replace('Room.html#Top');
+  location.assign('Room.html#Top');
 }
 
-function HostInput() {
-  hform.addEventListener("submit", (e) => {
+
+let hForm = document.getElementById('hostform');
+if(hForm) {
+  hForm.addEventListener("submit", (e) => {
     //e.preventDefault();
-    let data = new FormData(hform);
+    let data = new FormData(hForm);
     var object = {};
     data.forEach(function (value, key) {
       object[key] = value;
@@ -42,59 +42,60 @@ function HostInput() {
       if (response.ok) console.log("Host Form Sent");
     });
   })
-
+} else{
+  console.log("Host Form Failed");
 }
 
-function ClientInput() {
-  cForm.addEventListener("submit", (e) => {
-    //e.preventDefault();
-    let data = new FormData(cForm);
-    var object = {};
-    data.forEach(function (value, key) {
-      object[key] = value;
-    });
-    var jsondata = JSON.stringify(object);
-    var parsedjson = JSON.parse(jsondata);
-    const username = parsedjson["username"];
-    const serverip = parsedjson["serverip"];
-    console.log("username: "+username);
-    console.log("serverip: "+serverip);
-    fetch("/room/join", {
-      method: "POST",
-      body: new URLSearchParams({username, serverip}),
-    }).then((response) => {
-      if (response.ok) console.log("Client Form Sent");
-    });
-  })
+  let cForm = document.querySelector('#clientform');
 
-}
-
-
+  if (cForm){
+    cForm.addEventListener("submit", (e) => {
+      //e.preventDefault();
+      console.log("hi")
+      let data = new FormData(cForm);
+      var object = {};
+      data.forEach(function (value, key) {
+        object[key] = value;
+      });
+      var jsondata = JSON.stringify(object);
+      var parsedjson = JSON.parse(jsondata);
+      const username = parsedjson["username"];
+      const serverip = parsedjson["serverip"];
+      console.log("username: "+username);
+      console.log("serverip: "+serverip);
+      fetch("/room/join", {
+        method: "POST",
+        body: new URLSearchParams({username, serverip}),
+      }).then((response) => {
+        if (response.ok) console.log("Client Form Sent");
+      });
+    })
+  } else{
+    console.log("Client Form Failed");
+  }
+  
 
 function ClientInfoSubscribe(uri) {
-  
   var retryTime = 1000;
   function Connect(uri) {
     const events = new EventSource(uri);
     events.addEventListener("message", (ev) => {
-      console.log("message: ");
       console.log(ev);
       const msg = JSON.parse(ev.data);
-      console.log(msg);
-      //localStorage.setItem('msgs', msg);
-      //console.log("decoded data", JSON.stringify(msg));
-      //if (!"username" in msg && !"room_name" in msg && !"client_addr" in msg && !"idx" in msg) return;
-      //localStorage.setItem('room_name', msg.room_name);
-      //localStorage.setItem('username', msg.username);
-      //localStorage.setItem('client_addr', msg.client_addr);
-      //localStorage.setItem('idx', msg.idx);
+      if (!"username" in msg && !"room_name" in msg && !"client_addr" in msg && !"idx" in msg) return;
+      localStorage.clear();
+      localStorage.setItem('room_name', msg.room_name);
+      localStorage.setItem('username', msg.username);
+      localStorage.setItem('client_addr', msg.client_addr);
+      localStorage.setItem('idx', msg.idx);
       ChangePage();
     });
+
     events.addEventListener("open", () => {
       console.log(`connected to event stream at ${uri}`);
+      ChangePage();
       if (window.localStorage.length != 0) {
         retryTime = 1;
-        ChangePage();
       }
     });
 
@@ -108,5 +109,6 @@ function ClientInfoSubscribe(uri) {
   }
   Connect(uri);
 }
+
 
 ClientInfoSubscribe("/clientInfo");
